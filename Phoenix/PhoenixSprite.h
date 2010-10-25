09b392b38b39f05b2d19466716d11edb9e16376c
Sean Stacey <Sean Stacey@localhost>
2010-10-25 02:12:31 -0400

    new files

---
+++ Phoenix/PhoenixSprite.h
@@ -0,0 +1,34 @@
+#ifndef _PHOENIXSPRITE_H_
+#define _PHOENIXSPRITE_H_
+#include <tchar.h>
+#include "PhoenixTexture.h"
+
+
+namespace PhoenixCore{
+
+class PhSprite
+{
+private:
+  PhTextureManager* m_pTextureMan;
+  int*        m_pTextures;
+  int*        m_pTextureStart;
+  int*        m_pTextureEnd;
+  int         m_nDelay;
+  int         m_nSpriteIndex;
+  int         m_nSpriteLength;
+  int         m_nSpriteLoadIndex;
+
+
+public:
+  PhSprite(PhTextureManager* _pTexMan);
+  ~PhSprite();
+  bool SetSize(int _size);
+  void SetDelay(int _delay);
+  bool AddSprite(TCHAR* _filename);
+  PhTexture* GetNextSprite();
+  
+};
+
+};
+
+#endif
